cbbec9024cff9866039f46d46cede224a65b4e40
Armandocm19 <78453450+Armandocm19@users.noreply.github.com>
2021-02-09 17:26:49 -0600

    Update main.js
    Agregué interacción con un modal más

--- js/main.js
+++ js/main.js
@@ -41,3 +41,24 @@ window.addEventListener('click', function(e){
         modal3.style.display = 'none'
     }
 });
+
+let modal4 = document.getElementById('miModal4');
+let flex4 = document.getElementById('flex4');
+let abrir4 = document.getElementById('sass');
+let cerrar4 = document.getElementById('close4');
+
+abrir4.addEventListener('click', function(e){
+    e.preventDefault();
+    modal4.style.display = 'block';
+});
+
+
+cerrar4.addEventListener('click', function(){
+    modal4.style.display = 'none';
+});
+
+window.addEventListener('click', function(e){
+    if(e.target == flex4){
+        modal4.style.display = 'none'
+    }
+});
