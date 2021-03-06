d1d666d33714b7da1c0e1edf209f75d8c71e2005
Armandocm19 <78453450+Armandocm19@users.noreply.github.com>
2021-03-05 19:44:59 -0600

    Update main.js
    Le di la iteraccion a la ventana de proyectos

--- js/main.js
+++ js/main.js
@@ -62,3 +62,25 @@ window.addEventListener('click', function(e){
         modal4.style.display = 'none'
     }
 });
+
+
+let modal5 = document.getElementById('miModal5');
+let flex5 = document.getElementById('flex5');
+let abrir5 = document.getElementById('uno');
+let cerrar5 = document.getElementById('close5');
+
+abrir5.addEventListener('click', function(e){
+    e.preventDefault();
+    modal5.style.display = 'block';
+});
+
+
+cerrar5.addEventListener('click', function(){
+    modal5.style.display = 'none';
+});
+
+window.addEventListener('click', function(e){
+    if(e.target == flex5){
+        modal5.style.display = 'none'
+    }
+});
