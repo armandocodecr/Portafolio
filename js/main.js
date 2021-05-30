1cba6da2d533791ddc3cf99de0c0064135fed95e
Armandocm19 <78453450+Armandocm19@users.noreply.github.com>
2021-05-29 23:24:15 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -90,3 +90,30 @@ window.addEventListener('click', function(e){
         modal5.style.display = 'none'
     }
 });
+
+let modal6 = document.getElementById('miModal6');
+let flex6 = document.getElementById('flex6');
+let abrir7 = document.getElementById('uno2');
+let abrir8 = document.getElementById('uno3');
+let cerrar6 = document.getElementById('close6');
+
+abrir7.addEventListener('click', function(e){
+    e.preventDefault();
+    modal6.style.display = 'block';
+});
+
+ abrir8.addEventListener('click', function(e){
+    e.preventDefault();
+     modal6.style.display = 'block';
+ });
+
+
+cerrar6.addEventListener('click', function(){
+    modal6.style.display = 'none';
+});
+
+window.addEventListener('click', function(e){
+    if(e.target == flex6){
+        modal6.style.display = 'none'
+    }
+});
