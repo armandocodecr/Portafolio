ca8f3907bca28c57b71bb2daae1bf67560e48ddf
Armandocm19 <78453450+Armandocm19@users.noreply.github.com>
2021-12-08 00:41:02 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,3 @@
-window.addEventListener("load", function(){
-    const contenedor_loader = document.querySelector('.contenedor_loader');
-    contenedor_loader.style.opacity = 0;
-    contenedor_loader.style.visibility = 'hidden';
-});
-
 
 // Scroll
 
